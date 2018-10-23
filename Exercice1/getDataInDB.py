33992413d6635603c2666cd2294a13f30d167618
NE0Ghost <38914127+NE0Ghost@users.noreply.github.com>
2018-10-23 19:58:54 -0400

    Add files via upload

--- Exercice1/getDataInDB.py
+++ Exercice1/getDataInDB.py
@@ -1,3 +1,18 @@
 import sqlite3
+import pprint
 
+def create_connection(db_file):
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except sqlite3.Error as e:
+        print(e)
+        return None
+
+
+connexion = create_connection("spells.db")
+cursor = connexion.cursor()
+cursor.execute("SELECT name, level, components FROM spells WHERE (level <= 4 AND components = 'V')")
+
+pprint.pprint(cursor.fetchall())
 
